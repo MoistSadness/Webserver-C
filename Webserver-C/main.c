37e054c0d10a1205b26007821a84defb6179e675
ewaste <moistsadness@gmail.com>
2023-03-12 21:16:03 -0700

    Initial Commit

---
+++ Webserver-C/main.c
@@ -0,0 +1,14 @@
+//
+//  main.c
+//  Webserver-C
+//
+//  Created by E-Waste on 3/12/23.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
