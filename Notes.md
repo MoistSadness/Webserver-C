5fa36772472819ea5371d622bc4866e70f4202bf
ewaste <moistsadness@gmail.com>
2023-03-21 17:09:32 -0700

    Doing file I/O stuff

--- Notes.md
+++ Notes.md
@@ -4,3 +4,12 @@
 3. Listen on the port
 4. Accept requests from the internet
 5. Perform required actions
+
+## File IO has always been something I have had issues with when programming in C. This was a pretty good oportunity to sit down and get a bit more practice in, this time with practical applications
+- https://stackoverflow.com/questions/53054089/reading-content-from-a-file-and-storing-it-to-string-in-c
+
+
+
+
+
+
--- src/main.c
+++ src/main.c
@@ -1,46 +1,121 @@
 #include "stdio.h"
+#include "stdlib.h"
 #include "string.h"
 #include "errno.h" // errno.h provides an error number for whatever has gone wrong with the application
 #include "sys/socket.h"
+#include "sys/stat.h"
+#include "sys/types.h"
 #include "arpa/inet.h"
-#include <fcntl.h> // for open
-#include <unistd.h> // for close
+#include "fcntl.h" // for open
+#include "unistd.h" // for close
 
 #define PORT 8080
 #define BUFFER_SIZE 1024
 
-
-/* The request needs to be handled
-
+/*** Read an HTML file into memory
+ * Takes a path to the desired file as input
+ * Return a file pointer
+ * 
 */
-void handle_http_request(int newsockfd, char* method, char* uri, char* version, char* response){
-    // Run some code based on the method here, GET, POST, etc... A switch statement cannot be used with strings, so an if/else block will have to be set up with strcmp.
-    if (strcmp(method, "GET") == 0){
+char* readHtmlFile(char* pagesRoot){
+    //printf("Reading file\n");
+
+    char testfile[] = "./testfile.html";
+    FILE* fp;
+
+    struct stat st;     // Store file data here
+    if (stat(pagesRoot, &st) ==0) printf("File size is %d\n", st.st_size);      // Get the size of the file in bytes
+
+    // Dynamically allocate memory for file, read it into buffer and print it
+    char* fileBuffer = malloc(st.st_size * sizeof(char));
+    
+
+    // Free memory
+
+
+
+    return fileBuffer;
+}
+
+/*** Free dynamically allocated memory
+*/
+void closeHtmlFile(char* file){
+    free(file);
+    if(file){       // If the pointer is still allocated, send an error message
+        perror("Webserver (free)");
+    }
+}
+
+/*** The request needs to be handled
+ * Run some code based on the method here, GET, POST, etc... Basic CRUD functionality will be handled for now.
+ * 
+*/
+void handle_http_request(int newsockfd, char* method, char* uri, char* version){
+    char response[] = "HTTP/1.0 200 OK\r\n"
+        "Server: webserver-c\r\n"
+        "Content-type: text/html\r\n\r\n"
+        "<html>hello, world</html>\r\n";
+
+    // This is the root directory for the HTML page router 
+    char pagesRoot[] = "../pages/index.html";
+
+    // A switch statement cannot be used with strings, so an if/else block will have to be set up with strcmp.
+    if (strcmp(method, "GET") == 0){                                            // READ
         printf("Get request!\n");
-    } else if (strcmp(method, "POST") == 0){
+        // Serve html files based on what the URI is
+        //router(uri);
+    } 
+    else if (strcmp(method, "POST") == 0){                                      // CREATE
         printf("Post request!\n");
-    } else {
+    } 
+    else if (strcmp(method, "POST") == 0 || strcmp(method, "PUT") == 0){        // UPDATE
+        printf("Update request!\n");
+    } 
+    else if (strcmp(method, "DELETE") == 0){                                    // DELETE
+        printf("Delete request!\n");
+    } 
+    else {
         printf("Invalid request!\n");
     }
 
-    // Route the reqest based on the URI. Use a filesystem based routing system like in Next.js
+    fflush(stdout);
 
+
+    // Read HTML file
+    char* file = readHtmlFile(pagesRoot);
+
+
+   
     int socketWrite = write(newsockfd, response, strlen(response));
     if(socketWrite < 0){
         perror("Webserver (write)");
         return;
         //continue;
     }
+
+    // Close HTML file and free the used memory
+    closeHtmlFile(file);
 }
 
+/*** Route a GET reqest based on the URI. Use a filesystem based routing system like in Next.js
+ * Use the URI to generate a path to the desired HTML file, load it into memory and return a pointer to the string 
+ where it can be sent to the client or modified or whatver needs to be done
+ *      If file does't exists, send 404
+ * index.html will correspond to the root file in a directory
+*/
+char* router(char * uri){
+    printf("Routing!");
+    return uri;
+}
+
+
+/*** Set up sockets, addresses and application loop
+ * 
+ * 
+*/
 int main(){
     char buffer[BUFFER_SIZE];
-
-    char response[] = "HTTP/1.0 200 OK\r\n"
-        "Server: webserver-c\r\n"
-        "Content-type: text/html\r\n\r\n"
-        "<html>hello, world</html>\r\n";
-
+    
     // Create socket
     int sockfd = socket(AF_INET, SOCK_STREAM, 0);
     // Throw error if the socket creation fails
@@ -110,7 +185,7 @@ int main(){
         printf("%s %s %s \t from \t [%s:%u]\n",method, uri, version, inet_ntoa(client_addr.sin_addr), ntohs(client_addr.sin_port));  
 
         // Handle the request now that it has been confirmed and printed to screen
-        handle_http_request(newsockfd, method, uri, version, response);
+        handle_http_request(newsockfd, method, uri, version);
 
         close(newsockfd);
     }
